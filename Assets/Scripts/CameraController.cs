44e11cfdd9a8f85bf7c6fcecf99ad25b696da0ba
Dylan Adams <dylancadams1@gmail.com>
2019-04-08 20:21:43 -0400

    A more complex version of the game I created with SDL2 and Box2D.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public BoxCollider2D boundBox;
+
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private Vector3 offset;
+
+    private float halfHeight;
+    private float halfWidth;
+
+    // Use this for initialization
+    void Start ()
+    {
+        offset = transform.position - player.transform.position;
+        theCamera = GetComponent<Camera>();
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+        
+	}
+	
+	void LateUpdate ()
+    {
+        transform.position = player.transform.position + offset;
+
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Sprite playerRight;
+    public Sprite playerLeft;
+    public GameObject projectile;
+    public GameObject camera;
+    public ParticleSystem poof;
+
+    private Rigidbody2D rb2d;
+    private Vector3 pz;
+    private Vector3 testV;
+    private SpriteRenderer spriteRenderer;
+    private bool facingRight;
+
+    void Start()
+    {
+        facingRight = true;
+        rb2d = GetComponent<Rigidbody2D>();
+        camera = GetComponent<GameObject>();
+    }
+
+    void FixedUpdate()
+    {
+
+        /* ----------------- Movement -------------------*/
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        
+        rb2d.velocity = new Vector2(moveX * speed, moveY * speed);
+        /*----------------------------------------------*/
+
+        /* ----------------- Mouse Location ------------*/
+        pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pz.z = 0;
+        pz.x += .5f;
+        /*----------------------------------------------*/
+
+        /* -------------------Flip-----------------------*/
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (pz.x > rb2d.transform.position.x)
+            spriteRenderer.flipX = false;
+        else if (pz.x < rb2d.transform.position.x)
+            spriteRenderer.flipX = true;
+        /*----------------------------------------------*/
+
+        /* ------------- Teleportation ---------- */
+
+        if(Input.GetKeyDown("space"))
+        {
+            rb2d.transform.position = pz;
+            poof.transform.position = pz;
+            
+            poof.Play();
+        }
+
+        /*----------------------------------------------*/
+
+        /* ------------- Projectiles ---------- */
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newGO = new GameObject();
+            newGO = projectile;
+
+            newGO.transform.position = Vector2.MoveTowards(newGO.transform.position, new Vector2(rb2d.position.x + 20, rb2d.position.y), Time.deltaTime);
+
+        }
+
+        /*----------------------------------------------*/
+
+        //poof.Play();
+
+    }
+
+}
